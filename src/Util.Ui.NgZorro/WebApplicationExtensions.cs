d7d4a3bbca29ada6bb2d5659ec79dc4198fcfb74
刘剑锋 <willow.lau@outlook.com>
2023-01-13 17:56:43 +0800

    升级到最新框架

--- src/Util.Ui.NgZorro/WebApplicationExtensions.cs
+++ src/Util.Ui.NgZorro/WebApplicationExtensions.cs
@@ -4,29 +4,31 @@ using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Util.Ui.NgZorro {
+namespace Util.Ui.NgZorro
+{
     /// <summary>
     /// NgZorro配置扩展
     /// </summary>
-    public static class WebApplicationExtensions {
+    public static class WebApplicationExtensions
+    {
         /// <summary>
         /// 配置NgZorro应用
         /// </summary>
         /// <param name="app">Web应用</param>
         /// <param name="action">配置Spa操作</param>
-        public static WebApplication UseNgZorro( this WebApplication app, Action<ISpaBuilder> action ) {
-            app.CheckNull( nameof( app ) );
-            action.CheckNull( nameof( action ) );
+        public static WebApplication UseNgZorro(this WebApplication app, Action<ISpaBuilder> action)
+        {
+            app.CheckNull(nameof(app));
+            action.CheckNull(nameof(action));
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseRouting();
-            app.UseEndpoints( endpoints => {
-                if ( app.Environment.IsDevelopment() ) {
-                    endpoints.MapRazorPages();
-                    endpoints.MapControllerRoute( "default", "{controller}/{action=Index}/{id?}" );
-                }
-            } );
-            app.UseSpa( action );
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapRazorPages();
+                app.MapControllerRoute("default", "{controller}/{action=Index}/{id?}");
+            }
+            app.UseSpa(action);
             return app;
         }
 
@@ -35,13 +37,15 @@ namespace Util.Ui.NgZorro {
         /// </summary>
         /// <param name="app">Web应用</param>
         /// <param name="developmentServerBaseUri">开发服务器基地址,范例: http://localhost:5000</param>
-        public static WebApplication UseNgZorro( this WebApplication app, string developmentServerBaseUri ) {
-            app.CheckNull( nameof( app ) );
-            return app.UseNgZorro( spa => {
+        public static WebApplication UseNgZorro(this WebApplication app, string developmentServerBaseUri)
+        {
+            app.CheckNull(nameof(app));
+            return app.UseNgZorro(spa =>
+            {
                 spa.Options.SourcePath = "ClientApp";
-                if ( app.Environment.IsDevelopment() )
-                    spa.UseProxyToSpaDevelopmentServer( developmentServerBaseUri );
-            } );
+                if (app.Environment.IsDevelopment())
+                    spa.UseProxyToSpaDevelopmentServer(developmentServerBaseUri);
+            });
         }
     }
 }
